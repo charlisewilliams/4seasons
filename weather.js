df4beca162408cd3ad89a1de8f9240d169a1de8f
charlisewilliams <charlisejayna@gmail.com>
2016-08-05 11:33:45 -0700

    new changes

--- weather.js
+++ weather.js
@@ -31,8 +31,7 @@ function loadWeather(location, woeid) {
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+ 
       
       $("#weather").html(html);
     },
