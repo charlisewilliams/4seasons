20f1b8b9ac4aef823c7f7c13a7c23525a90d53c0
briannadixon <brianna.dixon.dahs2018@gmail.com>
2016-08-09 11:01:43 -0700

    fix weather buttons

--- weather.js
+++ weather.js
@@ -31,6 +31,7 @@ function loadWeather(location, woeid) {
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<li>'+weather.city+', '+weather.region+'</li>';
+      
       $("#weather").html(html);
     },
     error: function(error) {
